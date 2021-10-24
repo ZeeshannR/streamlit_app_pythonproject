6dda79b6f842d7d66bc027ad65bf0b6689daff68
ZeeshannR <93027818+ZeeshannR@users.noreply.github.com>
2021-10-24 16:22:28 +0530

    Update car_prediction_app.py

--- car_prediction_app.py
+++ car_prediction_app.py
@@ -16,6 +16,9 @@ from sklearn.model_selection import train_test_split
 #from sklearn.metrics import mean_squared_error,r2_score
 #from sklearn.metrics import r2_score
 import plotly.graph_objects as go
+import random
+from openpyxl.workbook import Workbook
+
 st.title('Used Car for Sale')
 st.text('Find the right car as per your choice')
 df=pd.read_csv('cleaned data.csv')
@@ -61,7 +64,7 @@ count=0
 pred_val=0
 for i in df_new.keys():
     try:
-        val=st.text_input("Enter no./Val of",+i)
+        val=st.text_input("Enter no./Val of "+i)
         pred_val=pred_val+float(val)*reg.coef_[count]
         count=count+1
     except:
@@ -69,10 +72,17 @@ for i in df_new.keys():
 st.text('Predicted prices are: ' +str(pred_val+reg.intercept_))
 st.header("Upload your queries")
 img=st.file_uploader("Upload")
+st.text('What should we call you?')
+name=st.text_input("Your name here:")
 st.text("Details for the representatives to contact you")
 st.text("Enter your address")
 address=st.text_area("Your address here")
 date=st.date_input('Enter a date')
+
 if st.checkbox("I confirm the date and time",value=False):
-    st.write("Thanks for confirming")
+    if st.button('Register!'):
+        userdict = {"Name": name, "Address":address, "Date":date}
+        userdf = pd.DataFrame(userdict.items())
+        userdf.to_excel(name + str(random.randint(1, 1000)) + ".xlsx")
+        st.write("Thanks for registering with us! We will get back to you on the mentioned details. Thanks for connecting.")
 st.number_input("Rate our site,min_value=1,max_value=10")
